4f45fe1487cbf847be88d0af9e7861e25747e87a
Atanu Roy <atanuroy911@gmail.com>
2023-05-28 10:40:02 +0530

    nam

--- yolov7/models/common.py
+++ yolov7/models/common.py
@@ -2600,3 +2600,38 @@ class CBAM(nn.Module):
         Ms = Ms.view(Ms.size(0), 1, Ms.size(2), Ms.size(3))
         Mf2 = Ms * Mf1
         return Mf2
+    
+
+# NAM Attention Model
+
+class Channel_Att(nn.Module):
+    def __init__(self, channels, t=16):
+        super(Channel_Att, self).__init__()
+        self.channels = channels
+      
+        self.bn2 = nn.BatchNorm2d(self.channels, affine=True)
+
+
+    def forward(self, x):
+        residual = x
+
+        x = self.bn2(x)
+        weight_bn = self.bn2.weight.data.abs() / torch.sum(self.bn2.weight.data.abs())
+        x = x.permute(0, 2, 3, 1).contiguous()
+        x = torch.mul(weight_bn, x)
+        x = x.permute(0, 3, 1, 2).contiguous()
+        
+        x = torch.sigmoid(x) * residual #
+        
+        return x
+
+
+class NAMAttention(nn.Module):
+    def __init__(self, channels, out_channels=None, no_spatial=True):
+        super(NAMAttention, self).__init__()
+        self.Channel_Att = Channel_Att(channels)
+  
+    def forward(self, x):
+        x_out1=self.Channel_Att(x)
+ 
+        return x_out1  
